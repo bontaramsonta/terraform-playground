086c888673029aedf533562e6d7761c8f56e6974
sourav dhar <dsourav@amalgamrx.com>
2025-04-11 14:47:48 +0530

    ssm try 01

--- main.tf
+++ main.tf
@@ -1,19 +1,30 @@
+variable "region" {
+  description = "The AWS region where resources will be created"
+  default     = "ap-south-1"
+}
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.94.1"
     }
   }
+  backend "s3" {
+    bucket       = "my-tf-state-3y5"
+    key          = "terraform/default-state"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
   default_tags {
     tags = {
       "managed_by" = "terraform",
-      "region"     = "ap-south-1",
+      "region"     = var.region,
       "project"    = "ec2-instance-connect"
     }
   }
@@ -33,6 +44,7 @@ resource "aws_subnet" "my_private_subnet" {
   availability_zone       = "ap-south-1a" # Update to your preferred AZ
   map_public_ip_on_launch = false
 }
+
 #! Debug
 output "default_vpc_id" {
   value = aws_vpc.my_vpc.id
@@ -65,7 +77,7 @@ data "aws_ami" "amazon_linux_2" {
 
   filter {
     name   = "architecture"
-    values = ["arm64"]
+    values = ["x86_64"]
   }
 
   filter {
@@ -89,24 +101,64 @@ output "ami-vol_type" {
   value       = one(data.aws_ami.amazon_linux_2.block_device_mappings).ebs.volume_type
   description = "ami vol type"
 }
-#!
+#---
 
-resource "aws_security_group" "ssm_example_sg" {
-  name   = "ssm-example-sg"
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    description     = "SSH from connect endpoint"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.endpoint_sg.id]
-  }
+# IAM stuff
+resource "aws_iam_role" "ssm_role" {
+  name = "SSM-Session-Manager-Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
 }
 
+# resource "aws_iam_policy" "ssm_policy" {
+#   name        = "SSM-Session-Manager-Policy"
+#   description = "Minimal policy to allow SSM Session Manager access"
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ssm:StartSession",
+#           "ssm:DescribeInstanceInformation",
+#           "ssmmessages:CreateControlChannel",
+#           "ssmmessages:CreateDataChannel",
+#           "ssmmessages:OpenControlChannel",
+#           "ssmmessages:OpenDataChannel",
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+resource "aws_iam_role_policy_attachment" "attach_ssm_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ssm_role.name
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "SSM-Instance-Profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# Instance
 resource "aws_instance" "ssm_example_instance" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t4g.micro"
+  ami                  = data.aws_ami.amazon_linux_2.id
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
   # For IMDv2
   metadata_options {
     http_endpoint = "enabled"
@@ -118,20 +170,50 @@ resource "aws_instance" "ssm_example_instance" {
   vpc_security_group_ids = [aws_security_group.ssm_example_sg.id]
 }
 
-resource "aws_security_group" "endpoint_sg" {
-  name   = "endpoint-sg"
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id            = aws_vpc.my_vpc.id
+  subnet_ids        = [aws_subnet.my_private_subnet.id]
+  service_name      = "com.amazonaws.${var.region}.ec2messages"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = aws_vpc.my_vpc.id
+  subnet_ids        = [aws_subnet.my_private_subnet.id]
+  service_name      = "com.amazonaws.${var.region}.ssmmessages"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.my_vpc.id
+  subnet_ids        = [aws_subnet.my_private_subnet.id]
+  service_name      = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_security_group" "ssm_example_sg" {
+  name   = "ssm-example-sg"
   vpc_id = aws_vpc.my_vpc.id
 
+  ingress {
+    description      = "Inbound SSH traffic from EC2 Instance Connect Endpoint"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Outbound HTTPS traffic to SSM"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
 resource "aws_ec2_instance_connect_endpoint" "my_endpoint" {
-  preserve_client_ip = true
-  subnet_id          = aws_subnet.my_private_subnet.id
-  security_group_ids = [aws_security_group.endpoint_sg.id]
+  subnet_id = aws_subnet.my_private_subnet.id
 }
